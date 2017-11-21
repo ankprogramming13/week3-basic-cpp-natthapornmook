85e8056183000cdf666d0a40291cd5bbaf5f7a9b
Unknown <mookmook02na@gmail.com>
2017-11-21 17:26:55 +0700

    ทำข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Mr." << "Natthaporn chanachai" << endl;
+  cout << "mook" << endl;
+  cout << "female" <<endl;
+  cout << "02 october 1997" << endl;
+  cout << "sleep" << endl;
+  cout << "single" << endl;
   return 0;
 }
